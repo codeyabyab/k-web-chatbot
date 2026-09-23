6f10cc1844ff4a4d7436dd29c5cce0761028887d
codeyabyab <kobyabyab@gmail.com>
2026-09-24 06:57:53 +0800

    Add loader component

---
+++ src/components/misc/Loader.jsx
@@ -0,0 +1,11 @@
+function Loader() {
+  return (
+    <>
+      <div className="flex justify-center items-center my-4 animate-pulse">
+        <div className="w-6 h-6 border-4 border-zinc-700 border-t-sky-500 rounded-full animate-spin"></div>
+      </div>
+    </>
+  );
+}
+
+export default Loader;
